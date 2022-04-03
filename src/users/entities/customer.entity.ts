ba33f77fff0cc4e3598f6deb018327b05f49a124
Ruben Rangel <rubenrang@gmail.com>
2022-04-03 16:44:19 -0500

    Relations One to One

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,6 +1,34 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
 export class Customer {
+  @PrimaryColumn()
   id: number;
+  @Column({ type: 'varchar', length: 255 })
   name: string;
+  @Column({ type: 'varchar', length: 255 })
   lastName: string;
+  @Column()
   phone: string;
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @OneToOne(() => User, (user) => user.customer, { nullable: true })
+  user: User;
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,6 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { Client } from 'pg';
 
@@ -13,6 +15,7 @@ export class UsersService {
     private productsService: ProductsService,
     private configService: ConfigService,
     @Inject('PG') private clientPg: Client,
+    @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
   private counterId = 1;
@@ -25,48 +28,59 @@ export class UsersService {
     },
   ];
 
-  findAll() {
-    const apiKey = this.configService.get('API_KEY');
-    const dbName = this.configService.get('DATABASE_NAME');
-    console.log(apiKey, dbName);
-    return this.users;
+  async findAll() {
+    // const apiKey = this.configService.get('API_KEY');
+    // const dbName = this.configService.get('DATABASE_NAME');
+    // console.log(apiKey, dbName);
+    // return this.users;
+
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    const user = this.users.find((item) => item.id === id);
-    if (!user) {
-      throw new NotFoundException(`User #${id} not found`);
-    }
-    return user;
+  async findOne(id: number) {
+    // const user = this.users.find((item) => item.id === id);
+    // if (!user) {
+    //   throw new NotFoundException(`User #${id} not found`);
+    // }
+    return await this.userRepository.findOne();
   }
 
-  create(data: CreateUserDto) {
-    this.counterId = this.counterId + 1;
-    const newUser = {
-      id: this.counterId,
-      ...data,
-    };
-    this.users.push(newUser);
-    return newUser;
+  async create(data: CreateUserDto) {
+    // this.counterId = this.counterId + 1;
+    // const newUser = {
+    //   id: this.counterId,
+    //   ...data,
+    // };
+    // this.users.push(newUser);
+    const newUser = this.userRepository.create(data);
+
+    return this.userRepository.save(newUser);
   }
 
-  update(id: number, changes: UpdateUserDto) {
-    const user = this.findOne(id);
-    const index = this.users.findIndex((item) => item.id === id);
-    this.users[index] = {
-      ...user,
-      ...changes,
-    };
-    return this.users[index];
+  async update(id: number, changes: UpdateUserDto) {
+    // const user = this.findOne(id);
+    // const index = this.users.findIndex((item) => item.id === id);
+    // this.users[index] = {
+    //   ...user,
+    //   ...changes,
+    // };
+    // return this.users[index];
+
+    const user = await this.findOne(id);
+    this.userRepository.merge(user, changes);
+
+    return this.userRepository.save(user);
   }
 
   remove(id: number) {
-    const index = this.users.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new NotFoundException(`User #${id} not found`);
-    }
-    this.users.splice(index, 1);
-    return true;
+    // const index = this.users.findIndex((item) => item.id === id);
+    // if (index === -1) {
+    //   throw new NotFoundException(`User #${id} not found`);
+    // }
+    // this.users.splice(index, 1);
+    // return true;
+
+    return this.userRepository.delete(id);
   }
 
   async getOrderByUser(id: number) {
